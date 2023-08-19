9da2bbc6a7007bf0802c2558db1b1635e55b3338
Yar Night <yaroslav239@gmail.com>
2023-08-19 10:33:17 +0300

    add page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/elements/navbar/navbar.component';
+import { HeaderComponent } from './components/blocks/header/header.component';
+import { FooterComponent } from './components/blocks/footer/footer.component';
 import { MainPageComponent } from './components/pages/main-page/main-page.component';
-import { LangMenuComponent } from './components/lang-menu/lang-menu.component';
+import { LangMenuComponent } from './components/elements/lang-menu/lang-menu.component';
+import { RoomsComponent } from './components/pages/rooms/rooms.component';
+import { ServicesComponent } from './components/pages/services/services.component';
+import { OffersComponent } from './components/pages/offers/offers.component';
+import { GalleryComponent } from './components/pages/gallery/gallery.component';
+import { NewsComponent } from './components/pages/news/news.component';
+import { ReviewsComponent } from './components/pages/reviews/reviews.component';
+import { ContactsComponent } from './components/pages/contacts/contacts.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +23,14 @@ import { LangMenuComponent } from './components/lang-menu/lang-menu.component';
     HeaderComponent,
     FooterComponent,
     MainPageComponent,
-    LangMenuComponent
+    LangMenuComponent,
+    RoomsComponent,
+    ServicesComponent,
+    OffersComponent,
+    GalleryComponent,
+    NewsComponent,
+    ReviewsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/blocks/header/header.component.ts
+++ src/app/components/blocks/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { LangMenuComponent } from '../lang-menu/lang-menu.component';
+
 
 @Component({
   selector: 'app-header',
