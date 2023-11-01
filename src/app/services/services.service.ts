e7c1473a33c5de7e708f662d838d59bcb2f25e0c
Yar Night <yaroslav239@gmail.com>
2023-11-01 19:06:51 +0300

    finish servises component

--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -11,10 +11,40 @@ export interface IService {
 const SERVICES: IService[] = [
   {
     link : "/gallery",
-    text: "Casino",
-    alt : "Casino",
-    src:  "assets/rus-flag.png"
+    text: "Laundry",
+    alt : "laundry",
+    src:  "assets/laundry.png"
   }, 
+  {
+    link : "/gallery",
+    text: "Parking place",
+    alt : "parking-sign",
+    src:  "assets/parking-sign.png"
+  },
+  {
+    link : "/gallery",
+    text: "24-hours support",
+    alt : "support",
+    src:  "assets/24-hours.png"
+  },
+  {
+    link : "/gallery",
+    text: "Happy meal",
+    alt : "cutlery",
+    src:  "assets/cutlery.png"
+  },
+  {
+    link : "/gallery",
+    text: "Cocktail-drinks",
+    alt : "cocktail-drink",
+    src:  "assets/cocktail-drink.png"
+  },
+  {
+    link : "/gallery",
+    text: "Sauna and spa",
+    alt : "bathtub",
+    src:  "assets/bathtub.png"
+  },
 ]
 
 @Injectable({
